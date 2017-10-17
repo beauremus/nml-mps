6499abfa00804d1f5037fd1cac656ebef974a0fe
Beau Harrison <beau@fnal.gov>
2017-10-17 13:37:44 -0500

    Working bars with adjustable threshold lines

---
+++ js/const.js
@@ -0,0 +1,29 @@
+const monitors = [102, 103, 104, 107, 113, 115, 120, 121, 124, 130, 441, 443, 450, 454, 457, 460, 463, 467, 470, 475, 480, 502, 503, 504, 508, 601, 603, 605, 610, 704]
+
+const types = ['integrated', 'gate', 'train']
+
+const dpm = new DPM()
+const acl = new ACLD()
+
+const margin = {
+  top: 50,
+  right: 70,
+  bottom: 50,
+  left: 10
+}
+
+const rect = {
+  width: 75,
+  max: 20000
+}
+
+const line = {
+  width: 6
+}
+
+const text = {
+  offset: 15
+}
+
+const width = 150 - margin.left - margin.right
+const height = 400 - margin.top - margin.bottom
---
+++ js/main.js
@@ -0,0 +1,176 @@
+function updateLoss(elementName) {
+  let parent = {}
+
+  if (typeof elementName === 'string') {
+    parent = d3.select(elementName)
+  } else {
+    parent = d3.select(this)
+  }
+
+  return (dpmData, dpmInfo) => {
+    let data = yScale(10)
+    if (dpmData.data > 10) data = yScale(dpmData.data)
+
+    parent.select('rect').transition(t)
+      .attr('height', height - data)
+      .attr('y', data)
+
+    parent.select('.data')
+      .attr('y', data + text.offset)
+      .text(dpmData.data.toFixed(2))
+
+    parent.select('.label')
+      .text(dpmInfo.name)
+  }
+}
+
+function updateThreshold(elementName, thresholdType) {
+  let parent = {}
+
+  if (typeof elementName === 'string') {
+    parent = d3.select(elementName)
+  } else {
+    parent = d3.select(this)
+  }
+
+  return (dpmData, dpmInfo) => {
+    parent.select(thresholdType).select('.line')
+      .attr('y1', yScale(dpmData.data))
+      .attr('y2', yScale(dpmData.data))
+
+    parent.select(thresholdType).select('.label')
+      .attr('y', yScale(dpmData.data))
+      .text(dpmInfo.name)
+
+    parent.select(thresholdType).select('.data')
+      .attr('y', yScale(dpmData.data) + text.offset)
+      .text(dpmData.data.toFixed(0))
+  }
+}
+
+const yScale = d3.scaleLinear()
+  .domain([0, rect.max]) // input
+  .range([height, 0]) // output
+
+const xScale = d3.scaleBand()
+  .domain([1]) // input
+  .range([0, width]) // output
+  .padding(.1)
+
+const t = d3.transition()
+  .delay(66)
+  .duration(1000)
+
+function dragStarted() {
+  d3.select(this).raise()
+  dpm.stop()
+}
+
+function dragged(number, name) {
+  return () => {
+    updateThreshold(`#LM${number + name[0].toUpperCase()}`, `.${name}`)({
+      data: yScale.invert(d3.event.y - margin.top)
+    }, {
+        name: d3.select(this).select('.label').text()
+      })
+  }
+}
+
+function dragStopped() {
+  const deviceName = d3.select(this).select('.label').text()
+  acl.run(`set ${deviceName} ${yScale.invert(d3.event.y - margin.top)}`)
+  dpm.start()
+}
+
+const dragBehavior = d3.drag()
+  .on('start', dragStarted)
+  .on('end', dragStopped)
+
+function generateDisplays(numbers, names) {
+  for (number of numbers) {
+    for (name of names) {
+      const svg = d3.select('body').append('svg')
+        .attr('id', `LM${number + name[0].toUpperCase()}`)
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+
+      const loss = svg.append('g')
+        .attr('class', 'loss')
+        .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+      loss.append('rect')
+        .attr('class', 'rect')
+        .attr('fill', 'blue')
+        .attr('x', xScale(1))
+        .attr('width', xScale.bandwidth())
+
+      loss.append('svg:text')
+        .attr('class', 'text')
+        .attr('class', 'data')
+        .attr('stroke', 'none')
+        .attr('fill', 'white')
+        .attr('text', '0.00')
+        .attr('text-anchor', 'middle')
+        .attr('x', xScale(1) + xScale.bandwidth() / 2)
+
+      loss.append('svg:text')
+        .attr('class', 'text')
+        .attr('class', 'label')
+        .attr('stroke', 'none')
+        .attr('fill', 'black')
+        .attr('text', 'Z:TEST')
+        .attr('text-anchor', 'middle')
+        .attr('x', xScale(1) + xScale.bandwidth() / 2)
+        .attr('y', height + text.offset)
+
+      const threshold = svg.append('g')
+        .attr('class', name)
+        .attr('transform', `translate(${margin.left}, ${margin.top})`)
+        .call(dragBehavior.on('drag', dragged(number, name)))
+
+      threshold.append('line')
+        .attr('class', 'line')
+        .attr('stroke', 'yellow')
+        .attr('stroke-width', line.width)
+        .style('stroke-opacity', 0.5)
+        .attr('x1', xScale(1))
+        .attr('x2', xScale(1) + xScale.bandwidth())
+
+      threshold.append('svg:text')
+        .attr('class', 'text')
+        .attr('class', 'label')
+        .attr('stroke', 'none')
+        .attr('fill', 'black')
+        .attr('text', 'Z:TEST')
+        .attr('x', xScale(1) + xScale.bandwidth())
+
+      threshold.append('svg:text')
+        .attr('class', 'text')
+        .attr('class', 'data')
+        .attr('stroke', 'none')
+        .attr('fill', 'black')
+        .attr('text', '0')
+        .attr('x', xScale(1) + xScale.bandwidth())
+    }
+  }
+}
+
+function generateRequests(numbers, names) {
+  for (number of numbers) {
+    for (name of names) {
+      const firstCapital = name[0].toUpperCase()
+      const id = 'LM' + number + firstCapital
+      dpm.addRequest(`N:${id}`, updateLoss(`#${id}`))
+      dpm.addRequest(`N:${id}.SETTING`, updateThreshold(`#${id}`, `.${name}`))
+    }
+  }
+}
+
+function updateMax(newValue) {
+  yScale.domain([0, newValue])
+}
+
+generateDisplays(monitors, types)
+generateRequests(monitors, types)
+
+dpm.start()
